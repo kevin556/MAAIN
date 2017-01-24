7fdd73a0b120cd82a763efafe935b75dc2ef78cc
Kevin Youna <kevin.youna@gmail.com>
2017-01-24 15:18:59 +0100

    second implementation of file_to_CLI_matrix now working.It may need some optimization

--- matrice.py
+++ matrice.py
@@ -8,10 +8,11 @@ class Matrice(object):
 		self.tableau_l = []
 		self.tableau_i = []
 		self.nb_sommet = 0
+		print(type(text))
 		if(isinstance(text,basestring)):
 			self.tableau_c,self.get_from_filename(text)
 		else:
-			get_from_matrice(text)
+			get_matrix_from_file(text)
 
 
 	def get_from_filename(self,fileName):
@@ -20,6 +21,8 @@ class Matrice(object):
 	def get_from_matrice(self,matrice_a):
 		self.tableau_c,self.tableau_l, self.tableau_i = get_all_table(matrice_a)
 		self.nb_sommet = matrice_a.shape[0]
+
+#a refaire pour des matrices stochastiques c'est a dire avec des frequences.
 
 	def get_all_table(matrice_a):
 		#on part du principe que la matrice est de taille n * n
@@ -40,36 +43,43 @@ class Matrice(object):
 def get_matrix_from_file(fileName):
 	nombre_sommet=0;nb_ligne = 0
 	table_c =[];table_l =[0];table_i =[]
-	element_courant = 0;element_courant_i = 0
-	tmp_chiffre = 0
 	f=open(fileName,'r')
 	lignes = f.readlines()
 	f.close()
-	for ligne in lignes:
+	chiffre_lu=[];chiffre_gauche=0;chiffre_droit=0;chiffre_stocke=-1
+	compteur_chiffre_gauche = 0
+	for i in range(0,len(lignes),1):
 		'''
 			pour eviter de prendre en compte les lignes en debut de fichier qui ne sont pas des chiffres
 		'''
-		if not (unicode(ligne.split("\t")[0]).isnumeric()):
+		if not (unicode(lignes[i].split("\t")[0]).isnumeric()):
 			continue
 		else:
-			tmp = ligne.split("\t")
-			if(tmp_chiffre == 0):
-				tmp_chiffre = tmp[0]
-			if(tmp[0] != tmp_chiffre):
-				table_l.append(element_courant_i)
-				nb_ligne+=1
-				if(nombre_sommet == 0):
-					nombre_sommet = element_courant_i
-				element_courant=0
+			chiffre_lu =lignes[i].strip("\n").strip('\r').split('	')
+			chiffre_gauche = chiffre_lu[0]
+			chiffre_droit = chiffre_lu[1]
+			#pour l\'initialisation
+			if chiffre_stocke == -1:
+				chiffre_stocke = chiffre_gauche
 
-			print("gauche %s droite %s")%(tmp[0],tmp[1])
-			table_c.append(tmp[0])
-			element_courant+=1
-			element_courant_i+=1
-			table_i.append(element_courant)
-			tmp_chiffre = tmp[0]
+			if chiffre_gauche != chiffre_stocke:
+				print 'compteur_chiffre_gauche %d chiffre_gauche %s chiffre_stocke %s' %(compteur_chiffre_gauche,chiffre_gauche,chiffre_stocke)
+				for j in range(0,compteur_chiffre_gauche ,1):
+					table_c.append(1/(float)(compteur_chiffre_gauche))
+				table_l.append(table_l[-1]+compteur_chiffre_gauche)
+				compteur_chiffre_gauche = 0
+				chiffre_stocke = chiffre_gauche
 
 
-	print ("sommet %d ligne %d") %(nombre_sommet,nb_ligne)
+			compteur_chiffre_gauche+=1
+
+			table_i.append(chiffre_droit)
+	print 'chiffre_stocke %s '%(chiffre_stocke)
+	for i in range(0,compteur_chiffre_gauche,1):
+		table_c.append(1/(float)(compteur_chiffre_gauche))
+	table_l.append(len(table_c))
+
+
+	print table_c
 	return table_c,table_l,table_i,nombre_sommet
 
--- tp1.py
+++ tp1.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python2.7
-
 import numpy as np
+from sys import argv
 
 from matrice import Matrice
 from vecteur import Vecteur
 
+
+
 a = np.array([[0,0,1,0],[2,3,0,4],[0,5,6,7],[0,0,0,0]])
+'''
 b = np.array([[0,3,5,8],[1,0,2,0],[0,0,0,0],[0,3,0,0]])
-d = np.array([[0,0,0,1/float(3)],[1/float(2),0,0,1/float(3)],[0,1,0,1/float(3)],[1/float(2),0,1,0]])
+d = np.array([[0,0,0,1/3],[1/2,0,0,1/3],[0,1,0,1/3],[1/2,0,1,0]])
 
 
 c = Vecteur([2,3,1,3])
-e = Vecteur(np.array([1,0,0,0]))
+e = Vecteur([1,0,0,0])
 
 tmp = Matrice(a)
 tmp2 = Matrice(b)
-'''
 print(c.calcul_matrice_by_vector(tmp))
 print(c.calcul_matrice_by_vector(tmp2))
 print(c.produit_transpose(tmp))
 print(c.produit_transpose(tmp2))
-'''
+print(e.produit_transpose(Matrice(d)))
 
-print(e.produit_transpose(d))
-
-
-'''
 print(tmp.tableau_c)
 print(tmp.tableau_l)
 print(tmp.tableau_i)
@@ -35,3 +33,9 @@ print(tmp2.tableau_i)
 '''
 #multiplie_matrice(a,a)
 '''print(init_res_empty_matrix(a,a))'''
+
+if(len(argv)==2):
+	print argv[1]
+	c = Matrice(argv[1])
+else:
+	d = Matrice(a)
