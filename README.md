dac2be159d66fb7fd7ed405d342a1f204031c16a
RAMI1991 <ramibenmhammed@gmail.com>
2017-02-07 16:02:03 +0100

    modification

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ tentative d'implémentation de l'algo d'un moteur de recherche.
 Membre du groupe:
 Kévin Youna
 Moncef Sekhsoukh
+RAMI BEN MOHAMED
--- pagerank.py
+++ pagerank.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python2.7
-
+import math
+from matrice import Matrice
 class pagerank(object):
 
 	def __init__(self,matrice_a,vecteur_b):
 	self.pagerank=[]
 	self.matrice = matrice_a
 	self.vecteur = vecteur_b
-	self.epsilon=0.0
+	self.vecteur = vacteur_c
+	self.epsilon=0.015
+	self.delta=0.0
+	self.d=1.5
 
 
-	calcul(matrice_r1,vecteur_b):
-		print "appel de calcul"
+
+	calcul_pagerank(x):
+		x=calcul_vectorz(matrice_a)
+		while self.epsilon> delta:
+
+			x=Matrice.produit_tranpose(matrice_a,x)
+			delta= calcul_norm(x,y)
+
+	return x
+
+
+			
 
 
 
-	calcul_pagerank():
-		while True:
-			if calcul(matrice_r1,vecteur_b) - calcul(matrice_r0,vecteur_b) < self.epsilon:
-				break
+	calcul_norm(y,z):
+
+		return math.hypot(y,z)
+
+
+		
