5be435affc48a6a9cc4c364c4ff51652c3e754c1
moncefsekh <moncefsekh@gmail.com>
2017-02-08 12:31:28 +0100

    PageRankZero is modified  also PageRankZap is done.

--- programme3.py
+++ programme3.py
@@ -4,8 +4,9 @@ from matrice_new import Matrice
 from vecteur import Vecteur
 
 
-def pagerank_zero(matrice,sommet_depart,epsilon):
+def pagerank_zap(matrice,epsilon,sommet_depart,d):
 	compteur = 0
+	n = matrice.nb_colonne
 	v0 = Vecteur(matrice.nb_colonne,sommet_depart)
 	v1 = Vecteur(matrice.nb_colonne,sommet_depart)
 	v2 = Vecteur(matrice.nb_colonne,sommet_depart)
@@ -18,12 +19,22 @@ def pagerank_zero(matrice,sommet_depart,epsilon):
 			v0.vecteur = m.produit_transpose(v1)
 		else:
 			break
+			
+	for i in range (0,len(v1.vecteur)):
+		v1.vecteur[i]=(d/n+(1-d)*v1.vecteur[i])
 
-if len(argv) == 4:
+	print "vecteur zap %s" %(v1.vecteur)
+
+
+
+if len(argv) == 5:
 	sommet_depart = int(argv[2])
 	epsilon = float(argv[3])
+	d=float(argv[4])
 	res = []
 	m = Matrice(argv[1])
-	pagerank_zero(m,sommet_depart,epsilon)
+	pagerank_zap(m,epsilon,sommet_depart,d)
 else:
-	print "usage ./programme fichier_source sommet_depart epsilon"
+	print "usage ./programme fichier_source sommet_depart epsilon zap"
+
+
