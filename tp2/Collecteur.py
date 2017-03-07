74a9abb1842b5214a37f70ee4b49f2f49d185e8c
moncefsekh <moncefsekh@gmail.com>
2017-03-07 14:10:09 +0100

    Collecteur finale

--- tp2/Collecteur.py
+++ tp2/Collecteur.py
@@ -10,6 +10,8 @@ import copy
 import re
 
 
+
+
 class Collecteur:
     mots = []
     titre_id = {}   #Structure (titre , id)
@@ -18,10 +20,6 @@ class Collecteur:
     graph_nutella ={} #structure {idpage : [liens en titre]} les titres on va les traduire en IDs via la structure titre_id
 
 
-    #quand est ce que c'est appele ?
-    def process_buffer(buf):
-        tnode = ET.fromstring(buf)
-        #pull it apart and stick it in the database
 
     def make_file_and_word_list_from_file(filename):
         get_word_file(filename)
@@ -30,46 +28,25 @@ class Collecteur:
     def import_dico(self,filename_dico = 'dictionnaire.txt'):
         self.mots = [line.rstrip('\n') for line in open(filename_dico)]   
 
-    '''def create_tmp_dico(self):
-        tmp = [0] * len(self.mots)
-        self.dico_tmp = dict(izip(self.mots, tmp))'''
     
     def do_the_harlem_shake(self,fileName):
-        #Moncef pense que ça ne plantera pas 
-        id_get = False;tmp = False;tmp2 = False
-        inputbuffer="";titre="";idp=""
-
-        with open(str(fileName),'rb') as inputfile:
-            append = False
-            for line in inputfile: 
-                if '<page>' in line or tmp:
-                    tmp = True
-                    #on entre dans la page
-                    if '<title>' in line and '</title>' in line:
-                        titre = line.replace('<title>',"").replace('</title>',"").strip()
-                    if '<id>' in line and '</id>' in line and not id_get:
-                        idp = line.replace('<id>',"").replace("</id>","").strip()
-                        self.titre_id[titre] = idp
-                        id_get = True
-                    if '<text>' in line and '</text>' in line:
-                        inputbuffer+= line.replace('<text>',"").replace('</text>',"").strip()
-                    
-                    elif '<text' in line or tmp2:
-                        tmp2 = True
-                        inputbuffer+= line.replace('<text>',"").replace("</text>","").strip()+" "
-                    
-                    if '</text>' in line and tmp2:
-                        inputbuffer+= line.replace('<text>',"").replace("</text>","").strip()+" "
-                        tmp2 = False
-
-                if '</page>' in line:
-                    print "titre %s \n"%(titre)
-                    print "id %s \n"%(idp)
-                    print "text %s \n"%(inputbuffer)
-                    self.analyse_page(idp,inputbuffer+" "+titre)
-                    self.graph_nutella[idp] = self.get_all_links(inputbuffer)
-                    id_get = False;tmp = False;tmp2 = False
-                    inputbuffer="";titre="";idp=""
+        titre="";idp="";inputbuffer="";
+        tag="{http://www.mediawiki.org/xml/export-0.10/}"
+        context = ET.iterparse(fileName, events=("start", "end"))
+        context = iter(context)
+        event, root = context.next()
+        for event, elem in context:
+            if event == "end" and elem.tag == tag+"page":
+                titre = elem.findtext(tag+"title").encode('utf-8')
+                idp = elem.findtext(tag+"id")
+                self.titre_id[titre] = idp
+                a = elem.find(tag+"revision")
+                inputbuffer = a.findtext(tag+"text").encode('utf-8')
+                print idp , titre
+                elem.clear()
+                self.analyse_page(idp,inputbuffer+" "+titre)
+                self.graph_nutella[idp] = self.get_all_links(inputbuffer)
+                titre="";idp="";inputbuffer="";
 
     #ICI J'AI RECUPERE TOUT LES LIENS , A MODIFIER !
     def get_all_links(self,text):
@@ -92,21 +69,28 @@ class Collecteur:
                         
     def analyse_page(self,idp,text_to_analyse):
         #print 'TEXT', text_to_analyse
-        page_length = len(str.split(text_to_analyse," "))
+        tmp = str.split(text_to_analyse," ")
+        page_length = len(tmp)
         #print 'TOTAL', page_length
+        for elem in tmp :
+            if elem not in self.mots:
+                tmp.remove(elem)
 
-        for mot in str.split(text_to_analyse," "):
-            if mot in self.mots:
-                #Si le mot existe dans la structure mot_page
-                if mot in self.mot_page:
-                    #Si l'ID de la page existe on incrémente SINON on rajoute l'id avec un compteur = 1
-                    if idp in self.mot_page[mot]:
-                        self.mot_page[mot][idp] += 1
-                    else:
-                        self.mot_page[mot][idp] = 1
-                else:   
-                    self.mot_page[mot] = {}
+        for mot in tmp:
+            #Si le mot existe dans la structure mot_page
+            if mot in self.mot_page:
+                #Si l'ID de la page existe on incrémente SINON on rajoute l'id avec un compteur = 1
+                #idp -> id de page
+                if idp in self.mot_page[mot]:
+                    self.mot_page[mot][idp] += 1
+                else:
                     self.mot_page[mot][idp] = 1
+                tmp.remove(mot)
+            else:   
+                self.mot_page[mot] = {}
+                self.mot_page[mot][idp] = 1
+                tmp.remove(mot)
+              
         dic = copy.deepcopy(self.mot_page.copy())
         self.mot_page_frequence = self.rami_money(page_length,dic)    
                 
@@ -132,7 +116,6 @@ class Collecteur:
         print '________________________________________________________________________________'
         print 'GRAPH_nutella_UPDATED', self.graph_nutella
 
-
                         
 #fonction a travailler car certains mots ne doivent pas être pris en compte ----> RAMI WHERE THE HELL ARE YOU ?
 def format_mot(mot):
@@ -140,10 +123,9 @@ def format_mot(mot):
 
 
 if __name__ == '__main__':
-    if(len(argv)==2):
+    if len(argv)==2 :
         a = Collecteur()
         a.import_dico()
-        #a.create_tmp_dico()
         a.do_the_harlem_shake(argv[1])
         a.resultat()
         a.update_graph()
