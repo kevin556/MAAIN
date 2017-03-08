ea0b5b1183d080713a682ab6a7d361c2f5d25a97
moncefsekh <moncefsekh@gmail.com>
2017-03-08 22:25:24 +0100

    granola

--- tp2/hello.py
+++ tp2/hello.py
@@ -3,24 +3,23 @@ from random import randint
 
 #structure mot-page
 
-d = {'mot1': {'idpage1': 1, 'idpage1': 2}, 'mot2': {'idpage1': 3, 'idpage2': 4}}
+d = {'mot1': {'idpage1': 1, 'idpage99': 2,'idpage100': 2}, 'mot2': {'idpage1': 3, 'idpage2': 4}}
 
 #l'access  aux elements se fait comme suit ...
-
-print d['mot1']           # {'idpage1': 1, 'idpage1': 2}
-print d['mot1']['idpage1']    # 1
-print d['mot2']['idpage1']   # 3
 
 
 #l'ajout d'element one by one
 
-d['mot3'] = {}
-d['mot3']['idpage1'] = 7
-d['mot3']['idpage2'] = 8
+def granola_de_prince(ded):
+    myfile = open("graph_granola.txt", "w+")
+    for key,value in ded.iteritems():
+        myfile.write(key+'\n')
+        for elem in value:
+            try:
+                myfile.write('\t'+elem+" "+str(ded[key][elem])+'\n')
+            except KeyError:
+                pass
 
-d['mot2']['idpage1'] += 1
 
 
-d['mot6'] = {}
-d['mot6']['idpagex'] = 10
-print d
+granola_de_prince(d)
